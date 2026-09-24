3993fdf0cebd5ff266b8fe3c82f573a21e786211
skrishna-code <krishnasutar026@gmail.com>
2026-09-24 10:00:02 +0530

    Add UserDetails component to display user info

---
+++ src/components/UserDetails.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import {
+  useParams,
+  Link
+} from "react-router-dom";
+
+function UserDetails() {
+  const { id } = useParams();
+
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Username: {user.username}</p>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
+
+      <Link to="/">Back</Link>
+    </div>
+  );
+}
+
+export default UserDetails;
